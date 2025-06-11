af7f574ddb27ed02eae3d3dd31daaac50e282570
Valerio Franchi <105786900+Siddha95@users.noreply.github.com>
2025-06-11 13:24:56 +0200

    homepage

--- rolls/urls.py
+++ rolls/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views  # Import the views from the current directory
 
 urlpatterns = [
-    path('', views.rolls),
-    path('contacts', views.contacts)  # Map the root URL to the rolls view
+    path('', views.rolls, name='home'),  # Map the root URL to the rolls view
+    path('contacts', views.contacts, name='contacts'),  # Map the root URL to the rolls view
     # You can add more paths here if needed
 ]
